e1a0f64db688b87456df1b12e32b79cd997effcc
nameofname <nameofemail@gmail.com>
2015-08-14 22:23:36 -0400

    Now I check image mime type.

--- gallery.js
+++ gallery.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
-//var imagesDir = express.static(__dirname + '../pics/inspiration/');
+var mime = require('mime');
 var imagesPath = __dirname + '/../pics/inspiration/';
 var fs = require('fs');
 
 app.use('/css', express.static(__dirname + '/css'));
 
-console.log(fs.realpathSync(imagesPath));
 app.use('/images', express.static(fs.realpathSync(imagesPath)));
 
 app.use('/js', express.static(__dirname + '/js'));
 
 app.set('views', __dirname + '/html');
-
-console.log('ronalding !!!!!!!!!!!!!!!!!!!!');
 
 app.engine('html', require('ejs').renderFile);
 
@@ -27,17 +24,21 @@ app.use('/ajax', function (req, res, next) {
     var offset = req.query.offset || 0;
     var limit = req.query.limit || 30;
     var fileName;
+    var fullPath;
+    var isImage;
     var arr = fs.readdirSync(imagesPath, function (err, files) {
         return files;
     });
 
     lewp:
     for (var idx = offset; idx < limit; idx++) {
-        if (arr[idx]) {
+        fullPath = imagesPath + arr[idx];
+        isImage = mime.lookup(fullPath).split('/')[0] === 'image';
+        if (arr[idx] && isImage) {
             fileName = prefix + arr[idx];
             out.push(fileName);
         }
-        if (idx === limit) {
+        if (arr.length === limit) {
             break lewp;
         }
     }
@@ -48,6 +49,5 @@ app.use('/ajax', function (req, res, next) {
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Image Gallery app listening at http://%s:%s', host, port);
 });
