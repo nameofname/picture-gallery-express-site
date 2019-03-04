42b37e4fdb0ed26d83c474cc577d6dbbe8baab9c
nameofname <nameofemail@gmail.com>
2019-03-04 00:04:08 -0500

    missing comma

--- app/conf.js
+++ app/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: 1312
-    imageDir : '/Users/ronald/Pictures/old HD pictures/iPhoto Library'
+    port: 1312,
+    imageDir : '/Users/ronald/Pictures/old HD pictures/iPhoto Library/2004'
     // imageDir : '/Users/ronald/Pictures/pics/illustrators\ and\ artists/',
     // imageDir : '/Users/ronald/Pictures/pics/inspiration/'
     // imageDir : '/Users/ronald/Pictures/pics/family\:friends\:life/all\ engagement\ pics/'
